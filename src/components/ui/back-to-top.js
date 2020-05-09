fab974d95b73d54aec6a11f5b346b280102024f5
McManus <macmanuch@hotmail.fr>
2020-05-09 02:16:48 -0400

    remove window ref

--- src/components/ui/back-to-top.js
+++ src/components/ui/back-to-top.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { navigate } from 'gatsby'
 import { AnchorLink } from 'gatsby-plugin-anchor-links'
 
 import { FaChevronUp } from 'react-icons/fa'
@@ -7,11 +6,9 @@ import { FaChevronUp } from 'react-icons/fa'
 import style from './back-to-top.module.scss'
 
 const BackToTop = () => {
-  const url = window.location.href
-  const slug = url.substr(url.lastIndexOf('/'), url.length)
   return (
     <div className={style.backToTop}>
-      <AnchorLink to={slug + '#hero'}>
+      <AnchorLink to={'#hero'}>
         <FaChevronUp className={style.chevron} />
       </AnchorLink>
     </div>
