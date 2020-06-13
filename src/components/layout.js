4a0063612242526c8cb44f4081f9ec8253ad44a6
McManus <macmanuch@hotmail.fr>
2020-06-12 23:51:22 -0400

    try root wrapper'

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,18 +3,15 @@ import { useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
 import Header from './header'
 import Footer from './footer'
-import Theme from './theme'
-import '../styles/main.scss'
 
 const Layout = ({ children }) => {
   const site = useSelector(state => state.site)
-
   return (
-    <Theme>
+    <>
       <Header site={site} />
       <main>{children}</main>
       <Footer footer={site.footer} />
-    </Theme>
+    </>
   )
 }
 
